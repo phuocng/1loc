3fe00434359829b8081d19f50efb4f44a3cc4108
Phuoc Nguyen <me@phuoc.ng>
2020-04-19 16:55:21 +0700

    Add your button

---
+++ client/AddYourButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const AddYourButton: React.FC<{}> = () => {
+    return (
+        <div className='bg-blue-100 border-2 border-dashed border-gray-400 flex items-center justify-center mt-3'>
+            <a 
+                href="https://github.com/phuoc-ng/1loc#contributing"
+                rel="noopener noreferrer"
+                target="_blank"
+                className='p-2 text-2xl text-center w-full'
+            >
+                Add your ...
+            </a>
+        </div>
+    );
+};
+
+export default AddYourButton;
