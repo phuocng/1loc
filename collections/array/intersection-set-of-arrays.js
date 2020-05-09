047c856c246c8950be5a1d9629d6c25ea8d254a5
Phuoc Nguyen <me@phuoc.ng>
2020-05-09 08:17:52 +0700

    Add examples

--- collections/array/intersection-set-of-arrays.js
+++
@@ -1,3 +0,0 @@
-// space: O(n)
-// time: O(n)
-const intersectionSetOfArrays = (...arrays) => [...(arrays.flat().reduce((map, v) => map.set(v, (map.get(v) || 0) + 1), new Map()))].reduce((acc, [v, count]) => void (count === arrays.length && acc.push(v)) || acc, []);
