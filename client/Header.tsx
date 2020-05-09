c1a89fd0185a3771cabe8596e3e3603752d04830
Phuoc Nguyen <me@phuoc.ng>
2020-05-09 14:58:44 +0700

    Add filter box

---
+++ client/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Filter from './Filter';
+
+const Header = () => {
+    return (
+        <div className='bg-white border-b border-gray-400 sticky top-0'>
+            <div className='ml-auto mr-auto max-w-3xl px-2 lg:px-0'>
+                <Filter />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- client/Layout.tsx
+++ client/Layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Header from './Header';
 import Footer from './Footer';
 
 const Layout: React.FC<{}> = ({ children }) => {
     return (
         <>
+            <Header />
             <div className='flex-1 px-2 lg:px-0'>
                 <div className="ml-auto mr-auto max-w-3xl mb-8">
                     {children}
