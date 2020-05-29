7abc538ad72a7f99d20d7753bdeba59e07e0d6c4
Phuoc Nguyen <me@phuoc.ng>
2020-05-29 17:18:44 +0700

    Tweak the header

--- client/layout/Header.tsx
+++ client/layout/Header.tsx
@@ -5,7 +5,7 @@ import Filter from '../components/Filter';
 const Header = () => {
     return (
         <div className='p-2 sticky top-0 z-10'>
-            <div className='ml-auto mr-auto max-w-3xl px-2 lg:px-0'>
+            <div className='ml-auto mr-auto max-w-3xl'>
                 <Filter />
             </div>
         </div>
