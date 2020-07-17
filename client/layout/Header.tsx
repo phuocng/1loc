8cf20a16ee35fe62fb4af01917c5f0f1c7e9d985
oniag <niag.souza1993@hotmail.com>
2020-07-16 23:45:41 -0300

    - Add background in filter div.

--- client/layout/Header.tsx
+++ client/layout/Header.tsx
@@ -4,7 +4,9 @@ import Filter from '../components/Filter';
 
 const Header = () => {
     return (
-        <div className='p-2 sticky top-0 z-10'>
+        <div className='p-2 sticky top-0 z-10' style={{
+            backgroundColor: '#FDCB6E'
+        }}>
             <div className='ml-auto mr-auto max-w-4xl'>
                 <Filter />
             </div>
