a8b643112ab9a9f09d08ab7f7c4ea5e6c4a84d06
Phuoc Nguyen <me@phuoc.ng>
2020-12-27 21:00:44 +0700

    Highlight code using prism

--- client/helpers/highlight.ts
+++ client/helpers/highlight.ts
@@ -1,18 +1,11 @@
-import hljs from 'highlight.js/lib/highlight';
-import css from 'highlight.js/lib/languages/css';
-import javascript from 'highlight.js/lib/languages/javascript';
-import html from 'highlight.js/lib/languages/xml';
-
-hljs.registerLanguage('css', css);
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('html', html);
+import Prism from 'prismjs';
 
 const highlight = (input: string, language: string) => {
     const lang = language || 'html';
-    const { value } = hljs.highlight(lang, input);
+    const value = Prism.highlight(input, Prism.languages[language], language);
     const highlighted = value.replace('&amp;', '&').trim();
 
-    return `<code class="${lang}">${highlighted}</code>`;
+    return `<code class="language-${lang}">${highlighted}</code>`;
 };
 
 export default highlight;
