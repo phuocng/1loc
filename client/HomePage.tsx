4df874a8648f0596f5e9a23b280073d305675366
Phuoc Nguyen <me@phuoc.ng>
2021-03-27 21:40:43 +0700

    Tweak layout

--- client/HomePage.tsx
+++ client/HomePage.tsx
@@ -1,59 +1,65 @@
 import React, { useEffect, useState } from 'react';
 
 import AddYourButton from './components/AddYourButton';
-import Separator from './components/Separator';
 import SnippetItem from './components/SnippetItem';
-import Layout from './layout/Layout';
 import snippets from './models/snippets';
 
 const HomePage = () => {
-    const [totalStars, setTotalStars] = useState(0);
+    const [totalStars, setTotalStars] = useState(4000);
     const total = snippets.map(item => item.snippets.length).reduce((a, b) => a + b, 0);
 
-    useEffect(() => {
-        fetch('https://api.github.com/repos/phuoc-ng/1loc')
-            .then(res => res.json())
-            .then(data => setTotalStars(data.stargazers_count))
-            .catch(console.log);
-    });
+    // useEffect(() => {
+    //     fetch('https://api.github.com/repos/phuoc-ng/1loc')
+    //         .then(res => res.json())
+    //         .then(data => setTotalStars(data.stargazers_count))
+    //         .catch(console.log);
+    // });
 
     return (
-        <Layout>
-            <h1 className='font-extrabold pt-24 text-center text-2xl sm:text-4xl px-1'>{total} Favorite JavaScript Utilities</h1>
-            <h3 className='font-light mb-16 text-2xl sm:text-3xl text-center'>
-                in <span className='font-bold' style={{ color: '#0984E3' }}>single line of code!</span> No more!
-            </h3>
+        <>
+            {/* Header */}
+            <header className="header">
+                <div className="container">
+                    <div className="header__logo">
+                        <img src="/assets/logo.png" alt="1 LOC" />
+                    </div>
+                    <h1 className="header__heading">{total} Favorite JavaScript Utilities</h1>
+                    <h3 className='header__subheading'>
+                        in single line of code! No more!
+                    </h3>
+                </div>
 
-            <div className="mb-48 text-center">
-                <a
-                    className="text-2xl text-white px-4 py-2 rounded-full"
-                    href="https://github.com/phuoc-ng/1loc"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    style={{
-                        backgroundColor: '#6C5CE7',
-                    }}
-                >
-                    GitHub ★ {totalStars}
-                </a>
-            </div>
+                <div className="header__github">
+                    <a
+                        href="https://github.com/phuoc-ng/1loc"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                    >
+                        GitHub ★ {totalStars}
+                    </a>
+                </div>
+            </header>
 
-            {
-                snippets.map((item) => {
-                    return (
-                        <div key={item.category} className='mb-16'>
-                            <div className='mb-4'>
-                                <Separator>{item.category.split('-').join(' ').toUpperCase()}</Separator>
-                            </div>
-                            {
-                                item.snippets.map((s) => <SnippetItem key={s.name} snippet={s} />)
-                            }
-                            <AddYourButton />
-                        </div>
-                    ); 
-                })
-            }
-        </Layout>
+            <main className="main">
+                <div className="container">
+                {
+                    snippets.map((item) => {
+                        return (
+                            <section key={item.category} className="section">
+                                <div className="section__separator">
+                                    <div className="section__title">{item.category.split('-').join(' ').toUpperCase()}</div>
+                                </div>
+                                {
+                                    item.snippets.map((s) => <SnippetItem key={s.name} snippet={s} />)
+                                }
+                                <AddYourButton />
+                            </section>
+                        ); 
+                    })
+                }
+                </div>
+            </main>
+        </>
     );
 };
 
--- client/layout/Layout.tsx
+++ client/layout/Layout.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
+import * as React from 'react';
 
-import Header from './Header';
+import Filter from '../components/Filter';
 import Footer from './Footer';
 
 const Layout: React.FC<{}> = ({ children }) => {
     return (
         <>
             <div className='relative'>
-                <div
-                    className='absolute top-0 left-0 right-0 bottom-0'
-                    style={{
-                        backgroundColor: '#FDCB6E',
-                        height: '32rem',
-                        transform: 'skewY(-4deg)',
-                        transformOrigin: 'top left',
-                        zIndex: -1,
-                    }}
-                />
-                <Header />
                 <div className='flex-1 px-2 lg:px-0'>
                     <div className="ml-auto mr-auto max-w-4xl mb-8">
                         {children}
