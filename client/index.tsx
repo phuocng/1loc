c0f73480157919a1ba8f217e38237c5368cfe3c0
Phuoc Nguyen <me@phuoc.ng>
2021-03-28 13:36:55 +0700

    Cleanup

--- client/index.tsx
+++ client/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { hydrate, render } from 'react-dom';
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
 rootElement.hasChildNodes()
-    ? hydrate(<App />, rootElement)
-    : render(<App />, rootElement);
+    ? ReactDom.hydrate(<App />, rootElement)
+    : ReactDom.render(<App />, rootElement);
