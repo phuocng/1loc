e9d5cd9673d995e8b493b03bca99dde837efe5cf
Phuoc Nguyen <me@phuoc.ng>
2021-03-28 09:35:13 +0700

    Move header to separate component

--- client/HomePage.tsx
+++ client/HomePage.tsx
@@ -1,46 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import * as React from 'react';
 
 import AddYourButton from './components/AddYourButton';
 import Snippet from './components/Snippet';
 import Footer from './layout/Footer';
+import Header from './layout/Header';
 import snippets from './models/snippets';
 
 const HomePage = () => {
-    const [totalStars, setTotalStars] = useState("____");
-    const total = snippets.map(item => item.snippets.length).reduce((a, b) => a + b, 0);
-
-    // useEffect(() => {
-    //     fetch('https://api.github.com/repos/phuoc-ng/1loc')
-    //         .then(res => res.json())
-    //         .then(data => setTotalStars(data.stargazers_count))
-    //         .catch(console.log);
-    // });
-
     return (
         <>
-            {/* Header */}
-            <header className="header">
-                <div className="container">
-                    <div className="header__logo">
-                        <img src="/assets/logo.png" alt="1 LOC" />
-                    </div>
-                    <h1 className="header__heading">{total} Favorite JavaScript Utilities</h1>
-                    <h3 className='header__subheading'>
-                        in single line of code! No more!
-                    </h3>
-                </div>
-
-                <div className="header__github">
-                    <a
-                        href="https://github.com/phuoc-ng/1loc"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                    >
-                        GitHub ★ {totalStars}
-                    </a>
-                </div>
-            </header>
-
+            <Header />
             <main className="main">
                 <div className="container">
                 {
@@ -60,8 +29,6 @@ const HomePage = () => {
                 }
                 </div>
             </main>
-
-            {/* Footer */}
             <Footer />
         </>
     );
---
+++ client/layout/Header.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+import * as React from 'react';
+
+import './header.css';
+import snippets from '../models/snippets';
+
+const Header: React.FC<{}> = () => {
+    const [totalStars, setTotalStars] = React.useState("____");
+    const total = snippets.map(item => item.snippets.length).reduce((a, b) => a + b, 0);
+
+    React.useEffect(() => {
+        fetch('https://api.github.com/repos/phuoc-ng/1loc')
+            .then(res => res.json())
+            .then(data => setTotalStars(data.stargazers_count))
+            .catch(console.log);
+    });
+
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="header__logo">
+                    <img src="/assets/logo.png" alt="1 LOC" />
+                </div>
+                <h1 className="header__heading">{total} Favorite JavaScript Utilities</h1>
+                <h3 className='header__subheading'>
+                    in single line of code! No more!
+                </h3>
+            </div>
+
+            <div className="header__github">
+                <a
+                    href="https://github.com/phuoc-ng/1loc"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
+                    GitHub ★ {totalStars}
+                </a>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
