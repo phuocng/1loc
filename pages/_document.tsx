8b08beaa5be1c33a814a86cdfbc72d4234a4f29c
Phuoc Nguyen <me@phuoc.ng>
2021-10-15 15:37:10 +0700

    Update favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,10 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link href="/favicon.png" rel="icon" />
                     <meta charSet="utf-8" />
+                    <link rel="icon" href="/favicon.png" type="image/png" />
+                    <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+                    <link rel="mask-icon" href="/mask-favicon.svg" color="#1975FF" />
                     <meta content="Favorite JavaScript utilities in single line of code" name="description" />
                     <meta content="JavaScript helpers, JavaScript snippets, JavaScript utils" name="keywords" />
                     <meta content="Nguyen Huu Phuoc" name="author" />
@@ -15,7 +17,6 @@ class MyDocument extends Document {
                     <meta content="summary" name="twitter:card" />
                     <meta name="twitter:title" content="Favorite JavaScript utilities in single line of code - 1 LOC" />
                     <meta name="twitter:description" content="Favorite JavaScript utilities in single line of code" />
-                    <meta content="/images/logo.png" name="twitter:image" />
 
                     <meta property="og:title" content="Favorite JavaScript utilities in single line of code - 1 LOC" />
                     <meta property="og:description" content="Favorite JavaScript utilities in single line of code" />
@@ -23,10 +24,8 @@ class MyDocument extends Document {
 
                     <meta content="article" property="og:type" />
                     <meta content="https://1loc.dev" property="og:url" />
-                    <meta content="/images/logo.png" property="og:image" />
                     <meta content="1LOC" property="og:site_name" />
 
-                    <link rel="icon" href="/images/favicon.png" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?&family=Source+Code+Pro:wght@400&family=Inter:wght@400;700&display=swap" />
