f5d3c22d3c406839fd9d72a5a2aa175b80abf2b0
elkarouani <elkarouani@gmail.com>
2022-01-29 19:22:43 +0100

    Task: make sidebar toggleable

---
+++ components/Icons/UpArrowIcon.tsx
@@ -0,0 +1,23 @@
+export default function UpArrowIcon(props: any) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			aria-hidden="true"
+			role="img"
+			width="1em"
+			height="1em"
+			preserveAspectRatio="xMidYMid meet"
+			viewBox="0 0 24 24"
+		>
+			<g fill="none">
+				<path
+					d="M4 15l8-8l8 8"
+					stroke="#161b33"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+				/>
+			</g>
+		</svg>
+	)
+}
